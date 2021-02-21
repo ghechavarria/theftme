5806c63e87b9090aa9bea0b16acfb06b448b74d8
Rafael Castillo <rafaelpntfs@gmail.com>
2021-02-21 04:57:50 -0500

    Added brick to load next scene. Added teleport points on floors of 3 scenes.

---
+++ Assets/NextScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)  //Unity function called when a collision is detected, and the object collided is put into the variable 'col' to be used later
+    {
+        if (col.gameObject.name == "THEFTED")   //if the object you collided with is the enemy
+        {
+            //transform.localScale += new Vector3(0, 0, 1); //increase the size of the ball
+            SceneManager.LoadScene(1);
+
+            //Destroy(col.gameObject);  //Destroy the enemy
+        }
+
+    }
+}
