43c269018915d2772a2e2f71b7031cf4f5e81975
mishakov7 <astaffers@gmail.com>
2021-02-21 03:19:07 -0500

    Populated dialogue objects, added UI/display scripts.

---
+++ Assets/Dialogue/Scripts/SpeakerUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeakerUI : MonoBehaviour
+{
+    new public Text name;
+    public Text dialogue;
+
+    private Character speaker;
+
+    // Tracks who the active speaker is.
+    public Character Speaker {
+        get { return speaker; }
+        set {
+            speaker = value;
+            name.text = speaker.name;
+        }
+    }
+
+    public string Dialogue {
+        set { dialogue.text = value;  }
+    }
+
+    public bool HasSpeaker() {
+        return speaker != null;
+    }
+
+    public bool SpeakerIs(Character character) {
+        return speaker == character;
+    }
+
+    public void Show() {
+        gameObject.SetActive(true);
+
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+}
